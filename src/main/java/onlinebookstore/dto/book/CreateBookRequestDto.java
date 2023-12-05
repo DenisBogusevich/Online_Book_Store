f8a01397715dd43d1c26b185e5c2b3108f99f5ee
Denis Bohusevich <bogusevich03@gmail.com>
2023-12-05 17:21:42 +0200

    test

--- src/main/java/onlinebookstore/dto/book/CreateBookRequestDto.java
+++ src/main/java/onlinebookstore/dto/book/CreateBookRequestDto.java
@@ -1,9 +1,9 @@
-package onlinebookstore.dto;
+package onlinebookstore.dto.book;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
-import lombok.Data;
+import java.util.Set;
 
 public record CreateBookRequestDto(
         @NotBlank
@@ -16,6 +16,7 @@ public record CreateBookRequestDto(
         @PositiveOrZero
         BigDecimal price,
         String description,
-        String coverImage) {
+        String coverImage,
+        Set<Long> categoryIds){
 }
 
